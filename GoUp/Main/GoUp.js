b84730ca2659ded6dc6abc88f3e89417b36598bc
ZhEgor <41516868+ZhEgor@users.noreply.github.com>
2019-03-29 15:44:53 +0200

    first

---
+++ GoUp/Main/GoUp.js
@@ -0,0 +1,28 @@
+//var goUpTXTElem = document.getElementById('goUpTXT');
+//var pageYLabel = 0;
+//window.onscroll = function() {
+//	var pageY = window.pageXOffset || document.documentElement.scrollTop;
+//	var innerHeight = document.documentElement.clientHeight;
+//	
+//	switch(goUpTXTElem.className){
+//	   case '':
+//			if(pageY > innerHeight){
+//				goUpTXTElem.className = 'up';
+//			}
+//			break;
+//			
+//		case 'up':
+//			if(pageY < innerHeight){
+//				goUpTXTElem.className = '';
+//		   	}
+//			break;
+//			
+//		case 'down':
+//			if(pageY > innerHeight){
+//				goUpTXTElem.className = 'up';
+//			}
+//			break;
+//	
+//	}
+//}; хз как делать, при скроллинеге вниз будет появяться текст. 
+
